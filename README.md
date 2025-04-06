0939a04945943c51ee88177873dd68f934673118
vicnotor <95343562+vicnotor@users.noreply.github.com>
2025-04-06 22:56:07 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# My Nix devshell templates
+
+Heavily inspired by and copied from
+[the-nix-way/dev-templates](https://github.com/the-nix-way/dev-templates).
---
+++ c/flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Simple clang devshell flake";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default =
+        pkgs.mkShell.override
+        {
+          stdenv = pkgs.clangStdenv;
+        }
+        {
+          packages = with pkgs; [
+            gcc
+            clang
+            clang-tools
+          ];
+          shellHook = ''
+            echo "Generating .clangd with correct include paths..."
+
+            INCLUDE_PATHS=$(clang -E -x c - -v < /dev/null 2>&1 \
+              | sed -n '/#include <...> search starts here:/,/End of search list./p' \
+              | grep -v '^#' \
+              | grep -v 'End of search list.' \
+              | sed 's/^ \{1,\}//' \
+              | sed 's/.*/"-isystem", "&"/' \
+              | paste -sd, -)
+
+            cat > .clangd <<EOF
+            CompileFlags:
+              Add: [''${INCLUDE_PATHS}]
+            EOF
+
+            echo ".clangd generated:"
+            cat .clangd
+          '';
+        };
+    });
+  };
+}
