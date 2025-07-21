90271b37709830c59adae3501be7a24df8c7a5de
vicnotor <95343562+vicnotor@users.noreply.github.com>
2025-07-21 18:58:20 +0200

    feat: added .gitignore files to each devenv shell

--- src/mydev.sh
+++ src/mydev.sh
@@ -21,18 +21,25 @@ if [ -f ./.envrc ]; then
   echo ".envrc already found. Moving existing .envrc to .envrc.bak."
   mv .envrc .envrc.bak
 fi
+cp "$template_dir"/"$template"/.envrc .
 
 if [ -f ./flake.nix ]; then
   echo "flake.nix already found. Moving existing flake.nix to flake.nix.bak"
   mv flake.nix flake.nix.bak
 fi
+cp "$template_dir"/"$template"/flake.nix .
 
 if [ -f ./flake.lock ]; then
   echo "flake.lock already found. Moving existing flake.lock to flake.lock.bak"
   mv flake.lock flake.lock.bak
 fi
+cp "$template_dir"/"$template"/flake.lock .
 
-cp "$template_dir"/"$template"/* .
+if [ -f ./flake.lock ]; then
+  echo ".gitignore already found. Not making any changes to it"
+else
+  cp "$template_dir"/"$template"/.gitignore .
+fi
+
 chmod +w *
-cp "$template_dir"/"$template"/.* .
 chmod +w .*
